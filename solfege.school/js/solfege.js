6f81e34e7a3842f08b32fd5ee935b98dc5631af0
solfegeschool <72528242+solfegeschool@users.noreply.github.com>
2020-10-08 14:52:27 +0900

    Add files via upload

---
+++ solfege.school/js/solfege.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+	var pagetop = $('.pagetop');
+	$(window).scroll(function () {
+		if ($(this).scrollTop() > 100) {
+			pagetop.fadeIn();
+		} else {
+			pagetop.fadeOut();
+		}
+	});
+	
+	$(function() {
+	    $("img").on('contextmenu', function(e) {
+	        return false;
+	    });
+	});    
+
+	$(function() {
+		$('a[href^=#]').click(function() {
+			var velocidad = 400;
+			var href = $(this).attr("href");
+			var target = $(href == "#" || href == "" ? 'html' : href);
+			var position = target.offset().top;
+			$('body,html').animate({scrollTop:position}, velocidad, 'swing');
+			return false;
+		});
+	});
+
+});
+
+
