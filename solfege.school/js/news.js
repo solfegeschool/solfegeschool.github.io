4ec2a9619cf29a4962ecfa51df299ad6f678ead9
solfegeschool <72528242+solfegeschool@users.noreply.github.com>
2024-06-22 11:36:21 +0900

    Add files via upload

--- solfege.school/js/news.js
+++ solfege.school/js/news.js
@@ -8,4 +8,26 @@ $(document).ready(function() {
  	});
 });
 
+var d = document;
+var highlights = d.querySelector('.highlights');
+var items = d.querySelector('.items');
+var itemCount = d.querySelectorAll('.item').length;
+var scroller = d.querySelector('.scroller');
+var pos = 0;
+var transform = Modernizr.prefixed('transform');
 
+function setTransform() {
+  items.style[transform] = 'translate3d(' + (-pos * items.offsetWidth) + 'px,0,0)';
+}
+
+function prev() {
+  pos = Math.max(pos - 1, 0);
+  setTransform();
+}
+
+function next() {
+  pos = Math.min(pos + 1, itemCount - 1);
+  setTransform();
+}
+
+window.addEventListener('resize', setTransform);
